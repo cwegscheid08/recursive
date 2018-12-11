7aa6fe9270cc08b2da574e8b3d24770493caa378
Chris Wegscheid <cwegscheid08@outlook.com>
2018-12-10 19:05:19 -0500

    Problem on odd number additions(array count, not Integer type)

--- recursion.rb
+++ recursion.rb
@@ -25,3 +25,41 @@ puts "FIBS_REC:\n 10:#{fibs_rec([], 10)}\n 30:#{fibs_rec([], 30)}\n 50:#{fibs_re
 # TIME IT IS RECALLED.
 
 # THE RECURSIVE METHOD IS GOING TO HAVE TO CONTIUALLY PUSH THE ARRAY BACK INTO THE METHOD AFTER IT HAS BEEN ALTERED.
+
+
+def merge_sort(ary)	
+	puts "ARY: #{ary.to_s}"
+	return ary if ary.count == 1
+	a = merge_sort(ary.slice!(0, (ary.count.to_i/2)))
+	b = merge_sort(ary)
+	sort = Array.new
+
+	puts "BEFORE SORTER: #{sort.to_s} A:#{a.to_s} B:#{b.to_s}"
+
+	b.count.times do |x|
+		puts "MIDDLE: #{sort.to_s} A:#{a.to_s} B:#{b.to_s} A[X]:#{a[x].class} B[X]:#{b[x].to_s} X:#{x}"
+		if a[0] == nil && b[0].class == Integer
+			sort << b.shift
+		elsif a[0] < b[0]
+			sort << a.shift
+			sort << b.shift
+		elsif b[0] < a[0]
+			sort << b.shift
+			sort << a.shift
+		else
+			sort << b.shift
+		end
+		puts "MIDDLE: #{sort.to_s} A:#{a.to_s} B:#{b.to_s} A[X]:#{a[x].class} B[X]:#{b[x].to_s} X:#{x}\n\n"
+	end
+
+	puts "AFTER SORTER: #{sort.to_s} A:#{a.to_s} B:#{b.to_s}"
+
+	return sort
+end
+
+array = Array.new
+rand(10).times { array.push(rand(10))}
+puts array.to_s
+
+puts "SORT: #{merge_sort([7, 4, 12, 1, 6, 0, 11, 3])}\nRANDOM ARRAY: #{merge_sort(array)}"
+
